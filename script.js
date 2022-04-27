124cbfc05127c015fbea636edd979738474901a4
Lee Wai Kin <leewaikin19@gmail.com>
2022-04-27 21:46:24 +0800

    a

--- script.js
+++ script.js
@@ -190,6 +190,7 @@ async function initQn() {
     }
 }
 function getQn() {
+    document.getElementById("question-img").removeChild(document.getElementById("question-img").firstChild);
     document.getElementById("question-img").appendChild(images[qn - 1]);
 }
 function changeQn(q) {
